9c1cdf600033a745dca86871c80f9833c00366b2
Daniel <ThePundik@gmail.com>
2024-01-05 22:20:16 +0300

    .

---
+++ heart2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+int main() {
+    std::string heart[] = {
+        "   @@@@      @@@@   ",
+        " @@@@@@@@  @@@@@@@@ ",
+        "@@@@@@@@@@@@@@@@@@@@",
+        " @@@@@@@@@@@@@@@@@@ ",
+        "  @@@@@@@@@@@@@@@@  ",
+        "    @@@@@@@@@@@@    ",
+        "      @@@@@@@@      ",
+        "        @@@@        ",
+        "         @@         "
+    };
+
+    for (const std::string& line : heart) {
+        std::cout << line << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+
+    return 0;
+}
